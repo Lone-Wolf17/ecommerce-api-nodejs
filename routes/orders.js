ad0a39f78e0f9150415ea43b4b95d393b6eb4ef0
lonewolf <adeyanju.akorede1@gmail.com>
2021-09-04 06:49:27 +0100

    ** Get Products Endpoint && Nesting Populate

--- routes/orders.js
+++ routes/orders.js
@@ -6,7 +6,9 @@ const router = express.Router();
 // @desc        Retrieve all Orders
 // @route       GET api/v1/orders
 router.get("/", async (req, res) => {
-  const orderList = await Order.find();
+  const orderList = await Order.find()
+    .populate("user", "name")
+    .sort({ dateOrdered: -1 });
 
   if (!orderList) {
     res.status(500).json({ success: false });
@@ -52,6 +54,22 @@ router.post("/", async (req, res) => {
     });
 
   res.status(200).send(order);
+});
+
+// @desc        Get Order By ID
+// @route       GET api/v1/orders/:id
+router.get("/:id", async (req, res) => {
+  const order = await Order.findById(req.params.id)
+    .populate("user", "name")
+    .populate({
+      path: "orderItems",
+      populate: { path: "product", populate: "category" },
+    });
+
+  if (!order) {
+    res.status(500).json({ success: false });
+  }
+  res.send(order);
 });
 
 module.exports = router;
