7ff31d180c73182682486e61d4bbe63d3ec5b64b
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2022-04-20 14:05:35 +0100

    ** ensure user email and phone number is unique

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,13 +7,13 @@ export class User {
   @Property({required: true})
   public name!: string;
 
-  @Property({required: true})
+  @Property({required: true, unique: true})
   public email!: string;
   
   @Property({required: true})
   public passwordHash!: string;
 
-  @Property({required: true})
+  @Property({required: true, unique: true})
   public phone!: string;
 
   @Property({default: false, required: true})
@@ -35,55 +35,3 @@ export class User {
   public country!: string;
 }
 export const UserModel = getModelForClass(User);
-// const UserSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//   },
-//   passwordHash: {
-//     type: String,
-//     required: true,
-//   },
-//   phone: {
-//     type: String,
-//     required: true,
-//   },
-//   street: {
-//     type: String,
-//     default: "",
-//   },
-//   apartment: {
-//     type: String,
-//     default: "",
-//   },
-//   city: {
-//     type: String,
-//     default: "",
-//   },
-//   zip: {
-//     type: String,
-//     default: "",
-//   },
-//   country: {
-//     type: String,
-//     default: "",
-//   },
-//   isAdmin: {
-//     type: Boolean,
-//     default: false,
-//   },
-// });
-
-// UserSchema.virtual("id").get(function () {
-//   return this._id.toHexString();
-// });
-
-// UserSchema.set("toJSON", {
-//   virtuals: true,
-// });
-
-// module.exports = mongoose.model(UserModelName, UserSchema);
--- src/util/db.ts
+++ src/util/db.ts
@@ -5,6 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI!, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      autoIndex: true
     } as ConnectOptions);
 
     console.log(`Mongooose Connected: ${conn.connection.host}`);
